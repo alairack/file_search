363a4f80586a3443167a1394b2751b7b4082bc4e
宋泰来 <1477115009@qq.com>
2022-03-15 09:13:55 +0800

    1.增加路径选择功能 2.增加右键菜单 3.修复了程序退出但线程仍驻留的问题

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import sys
 import os
 from PyQt5 import QtGui, QtWidgets, Qt, QtCore
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QMessageBox, QComboBox
-from file_search import search_file
+import time
 import pickle
+import ctypes
+import resource
+import subprocess
+from functools import partial
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -16,13 +20,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.search_frame = QtWidgets.QLineEdit()
         self.search_frame.setAlignment(QtCore.Qt.AlignLeft)
 
-        search_button = QtWidgets.QPushButton("搜索")
-        search_button.clicked.connect(self.click_search_button)
+        self.search_button = QtWidgets.QPushButton("搜索")
+        self.search_button.clicked.connect(self.click_search_button)
+
+        self.choose_search_dir_button = QtWidgets.QPushButton("选择路径")
+        self.choose_search_dir_button.clicked.connect(self.choose_search_dir)
 
         self.combobox = QComboBox(self)
         self.combobox.setLineEdit(self.search_frame)
         self.combobox.setMinimumWidth(self.width()-60)
         self.combobox.setMinimumHeight(26)
+        self.combobox.setCompleter(None)
         self.combobox.activated.connect(self.choose_history)
 
         view = QtWidgets.QListView(self.combobox)
@@ -31,9 +39,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.combobox.setView(view)
 
         hbox_layout = QtWidgets.QHBoxLayout()
-        hbox_layout.setSpacing(30)
-        hbox_layout.addWidget(self.combobox, alignment=QtCore.Qt.AlignRight)
-        hbox_layout.addWidget(search_button, alignment=QtCore.Qt.AlignRight)
+        hbox_layout.addWidget(self.combobox, stretch=0, alignment=QtCore.Qt.AlignRight)
+        hbox_layout.addWidget(self.choose_search_dir_button, alignment=QtCore.Qt.AlignRight)
+        hbox_layout.addWidget(self.search_button, stretch=0, alignment=QtCore.Qt.AlignRight)
+        hbox_layout.addStretch(10)
 
         vbox_layout = QtWidgets.QVBoxLayout()
         vbox_layout.setSpacing(15)
@@ -45,9 +54,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.table_widget.setColumnCount(4)
         self.table_widget.setHorizontalHeaderLabels(["文件名", "路径", '创建时间', "修改时间"])
         self.table_widget.setColumnWidth(0, 200)
-        self.table_widget.setColumnWidth(1, 300)
+        self.table_widget.setColumnWidth(1, 400)
         self.table_widget.setColumnWidth(2, 250)
         self.table_widget.doubleClicked.connect(self.table_widget.open_click_file)
+        # 右键菜单策略
+        # self.table_widget.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.DefaultContextMenu)
 
         widget = QtWidgets.QWidget()
         widget.setLayout(vbox_layout)
@@ -57,17 +68,26 @@ class MainWindow(QtWidgets.QMainWindow):
         vbox_layout.addWidget(self.table_widget)
         self.setCentralWidget(widget)
         self.history = History(self.combobox)
+        self.search_path = None
+        self.file_search_thread = None
 
     def click_search_button(self):
+        self.search_button.setEnabled(False)
+        self.search_button.setText("正在搜索")
         self.history.save_history(self.search_frame.text())
         file_name = self.search_frame.text()
         if file_name == "":
             QMessageBox.warning(self, "警告", "你还没有输入文件名", buttons=QMessageBox.Ok)
         else:
             self.threadpool = QtCore.QThreadPool()
-            self.file_search_thread = FileSearchThread(file_name)
+            self.file_search_thread = FileSearchThread(file_name, self.search_path)
             self.threadpool.start(self.file_search_thread)
             self.file_search_thread.signals.finished.connect(self.file_search_finish)
+
+    def choose_search_dir(self):
+        dir_choose = QtWidgets.QFileDialog.getExistingDirectory(self, "选取搜索文件夹")
+        if dir_choose != "":
+            self.search_path = dir_choose
 
     def file_search_finish(self):
         self.table_widget.clearContents()
@@ -82,10 +102,19 @@ class MainWindow(QtWidgets.QMainWindow):
                     table_cell.setIcon(file_icon)
                 self.table_widget.setItem(i, j, table_cell)
         self.statusBar().showMessage(f"{len(self.file_search_thread.result)}个对象")
+        self.search_button.setText("搜索")
+        self.search_button.setEnabled(True)
+        self.file_search_thread.is_running = False
 
     def choose_history(self, event):
         choose_text = self.combobox.itemText(event)
         self.search_frame.setText(choose_text)
+
+    def closeEvent(self, event):
+        event.accept()
+        if self.file_search_thread is not None:
+            if self.file_search_thread.is_running:
+                self.file_search_thread.is_running = False
 
 
 class ShowResultsTable(QtWidgets.QTableWidget):
@@ -107,18 +136,66 @@ class ShowResultsTable(QtWidgets.QTableWidget):
             except Exception:
                 pass
 
+    def open_folder(self, index):
+        row = index.row()
+        try:
+            file_path = self.item(row, 1).text()
+            cmd = 'explorer.exe /select, "{}", vbNormalFocus'.format(file_path)
+            si = subprocess.STARTUPINFO()
+            si.wShowWindow = subprocess.SW_HIDE
+            subprocess.call(cmd, startupinfo=si)
+        except Exception:
+            pass
+
+    def contextMenuEvent(self, event):
+        index = self.currentIndex()
+        menu = QtWidgets.QMenu(self)
+
+        default_open_file = QtWidgets.QAction("使用默认应用打开文件")
+        default_open_file.triggered.connect(partial(self.open_click_file, index))
+        open_dir = QtWidgets.QAction("打开文件目录")
+        open_dir.triggered.connect(partial(self.open_folder, index))
+
+        menu.addAction(default_open_file)
+        menu.addAction(open_dir)
+        menu.exec(event.globalPos())
+
 
 class FileSearchThread(QtCore.QRunnable):
-    def __init__(self, file_name):
+    def __init__(self, file_name, search_path):
         super(FileSearchThread, self).__init__()
         self.file_name = file_name
+        self.search_path = search_path
         self.result = None
+        self.is_running = True
         self.signals = Signals()
 
     @QtCore.pyqtSlot()
     def run(self):
-        self.result = search_file(self.file_name, [])
+        self.result = self.search_file(self.file_name, self.search_path)
         self.signals.finished.emit()
+
+    def search_file(self, name, path):
+        result = []
+        if path is None:
+            path = "/"
+        for root, folder, files in os.walk(path):
+            if not self.is_running:
+                break
+            for file_name in files:
+                if name in file_name:
+                    if not self.is_running:
+                        break
+                    file_path = os.path.abspath(os.path.join(root, file_name))
+
+                    stat_info = os.stat(file_path)
+
+                    # 时间戳转换
+                    create_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(stat_info.st_ctime))
+                    modif_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(stat_info.st_mtime))
+
+                    result.append([file_name, file_path, create_time, modif_time])  # 保存路径与盘符
+        return result
 
 
 class History(object):
@@ -174,7 +251,8 @@ class Signals(QtCore.QObject):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon("logo.ico"))
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("alairack")
+    app.setWindowIcon(QtGui.QIcon(":/icons/logo.ico"))
     main_window = MainWindow(None)
 
     main_window.show()
